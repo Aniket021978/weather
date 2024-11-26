b236770cdfa9a35e2ba216bc0b23a1a19d1daeec
aniket021978 <aniket021978@gmail.com>
2024-11-26 13:42:07 +0530

    all

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+// import Weather from './components/Weather.jsx';
+import Next from './components/Next.jsx';
+function App() {
+  return (
+    <>
+    {/* <Weather/> */}
+    <Next/>
+    </>
+  );
+}
+
+export default App;
